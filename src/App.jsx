18e47f43d1b237bd3667d3230c085ecae7742b4a
Josue <jasovi@gmail.com>
2023-08-26 16:32:56 -0500

    app clima primeros ajustes

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='ctn'>
+      <div className='head'>
+        <div className='headSection'>search</div>
+        <div className='headSection'>icono</div>
+        <div className='headSection'>15 grados</div>
+        <div className='headSection'>tiempo</div>
+        <div className='headSection'>fecha</div>
+        <div className='headSection'>lugar</div>
+      </div>
+      <div className='main'>
+        <h1>mundo</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
