ba9ace49b560214e3c8477d8594468cdf7e6c350
Josue <jasovi@gmail.com>
2023-08-26 19:37:03 -0500

    diseño mobile en desarrollo

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,26 @@ function App() {
         <div className='headSection'>lugar</div>
       </div>
       <div className='main'>
-        <h1>mundo</h1>
+        <div className='mainUp'>
+          <div className='mainUp1'>C° F°</div>
+          <div className='mainUp2'>
+            <div className='bloque'>Tomorrow</div>
+            <div className='bloque'>Sun, 7 Jun</div>
+            <div className='bloque'>Mon, 8 Jun</div>
+            <div className='bloque'>Tue, 9 Jun</div>
+            <div className='bloque'>Wed, 10 Jun</div>
+          </div>
+        </div>
+        <div className='mainDown'>
+          <h1>Today’s Hightlights</h1>
+          <div className='mainDownBloques'>
+            <div className='bloque1'>Wind status</div>
+            <div className='bloque1'>Humudity</div>
+            <div className='bloque2'>Visibility</div>
+            <div className='bloque2'>Air Pressure</div>
+          </div>
+        </div>
+        <div className='mainFoter'>usermane</div>
       </div>
     </div>
   )
